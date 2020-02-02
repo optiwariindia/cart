d89ac2f06e955d7297e0684e090b46bf36b940d3
Om Prakash Tiwari <om@localhost.localdomain>
2020-02-02 22:31:52 +0530

    Cart Interface

---
+++ src/cart.php
@@ -0,0 +1,23 @@
+<?php
+namespace optiwariindia;
+class cart{
+    private $cart=[];
+    public function __Construct(){
+
+    }
+    public function add($item){
+
+    }
+    public function remove($item){
+
+    }
+    public function update(){
+
+    }
+    public function count(){
+
+    }
+    public function get(){
+
+    }
+}
