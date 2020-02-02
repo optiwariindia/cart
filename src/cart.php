994a6a5a893586e16723c9b26b44eea5a3e35637
Om Prakash Tiwari <om@localhost.localdomain>
2020-02-02 23:08:43 +0530

    Version 1.0

--- src/cart.php
+++ src/cart.php
@@ -1,23 +1,77 @@
 <?php
 namespace optiwariindia;
-class cart{
-    private $cart=[];
-    public function __Construct(){
 
+class cart
+{
+    private $cart = [],$param=[];
+    public function __Construct()
+    {
+        // checking if session is started
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (isset($_SESSION['cart'])) {
+            $this->cart = $_SESSION['cart'];
+        }
     }
-    public function add($item){
-
+    public function setParams($param=[]){
+        $this->param=$param;
     }
-    public function remove($item){
-
+    public function add($item)
+    {
+        $update=true;
+        foreach ($this->cart as $key => $value) {
+            $match=true;
+            foreach ($this->param as $param) {
+                if($match && ($value[$param]==$item[$param])){
+                    $match=true;
+                }else{
+                    $match=false;
+                }
+            }
+            if($match && $update){
+                $value['count']+=$item['count'];
+                $update=false;
+                $this->cart[$key]=$value;
+            }
+        }
+        if($update){
+            $this->cart[]=$item;
+        }
+        $this->update();
     }
-    public function update(){
-
+    public function remove($item)
+    {
+        foreach ($this->cart as $key => $value) {
+            $match=true;
+            foreach ($this->param as $param) {
+                if($match && ($value[$param]==$item[$param])){
+                    $match=true;
+                }else{
+                    $match=false;
+                }
+            }
+            if($match){
+                unset($this->cart[$key]);
+            }
+        }
+        $this->update();
     }
-    public function count(){
-
+    public function update()
+    {
+        $_SESSION['cart']=$this->cart;
     }
-    public function get(){
-
+    public function count()
+    {
+        return array_reduce(
+            $this->cart,
+            function ($a,$b){
+                return $a+$b['count'];
+            },
+            0);
+    }
+    public function get()
+    {
+        return $this->cart;
     }
 }
